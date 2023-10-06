aea8b6a3be010ae554852d0ea7061fc02a8df7c2
Lauty821 <LautarodeFCBarcelona@outlook.com>
2023-10-06 15:18:05 -0300

    Formulario de publicación

---
+++ alumno.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Urquiza 49</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link" href="home-admin.php">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">Bolsa de trabajo</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="cerrar.php">Cerrar sesión</a>
+            </li>
+            
+        </ul>
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="jumbotron">
+           
+            <hr class="my-2">
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
